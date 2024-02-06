58521c7d9d1e121b52e0b47d7ec7716697652f22
Pranav Soni <pranavsoni1101@gmail.com>
2024-02-06 11:37:47 +0530

    Added sidebar and modified view pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { Heading, Text } from '@chakra-ui/react';
 import React from 'react';
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
 
 const App = () => {
     return(
--- src/Pages/ViewPdf.jsx
+++ src/Pages/ViewPdf.jsx
@@ -1,14 +1,70 @@
-import { Container, Heading } from '@chakra-ui/react';
-import React from 'react';
+import { Heading, Box, Button, Text, Flex, Grid, GridItem } from '@chakra-ui/react';
+import { Page, Document } from 'react-pdf';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import Sidebar from '../../components/Sidebar';
 
 const ViewPdf = () => {
+    let { id } = useParams();
+    const [file, setFile] = useState(null);
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    // useEffect(()=>{
+    //     fetchPdf();
+    // }, [])
+    
+    // const fetchPdf = async () => {
+    //     await axios.get(`http://localhost:3001/pdf/${id}`)
+    //     .then(response => {
+    //         // console.log("Success in fetching", response.data);
+    //         const fileUrl = URL.createObjectURL(response.data);
+    //         setFile(fileUrl)
+    //     })
+    //     .catch(err=> console.log("Error in fetching", err))
+    // }
+
+    const handleDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    };
+
     return(
         <>
-            <Container
-                maxW= "100%"
+            <Sidebar>
+            <Heading>This is edit pdf</Heading>
+            <Box
+                bg = "grey"
+                maxH={"4em"}
             >
-                <Heading>This is view PDF</Heading>
-            </Container>
+                <Text display={"inline-block"}>{pageNumber} of {numPages}</Text>
+
+            </Box>
+            <Flex
+                bg = "#00000099"
+                display = "flex"
+                justifyContent= "center"
+                alignItems = "center"
+                // w = "50em"
+            >
+                <Document
+                    file={`http://localhost:3001/pdf/${id}`}
+                    onLoadSuccess={handleDocumentLoadSuccess}
+                >
+                    {Array.from(new Array(numPages), (el, index) => (
+                        <Box
+                            my= "12px"
+                            key={`page_${index + 1}`}
+                        >
+                            <Page  pageNumber={index + 1} />
+                        </Box>
+                    ))}
+                </Document>
+            </Flex>
+            </Sidebar>
         </>
     )
 }

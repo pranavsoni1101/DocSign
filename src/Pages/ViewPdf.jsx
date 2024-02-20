54872f1732835d665a46d0f820693baadb0dbcb4
Pranav Soni <pranavsoni1101@gmail.com>
2024-02-20 13:51:45 +0530

    Modified view pdf to change the drag and drop logic

--- src/Pages/ViewPdf.jsx
+++ src/Pages/ViewPdf.jsx
@@ -19,16 +19,16 @@ const ViewPdf = () => {
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
     const [usersLoading, setUsersLoading] = useState(true); 
+    const [inputFields, setInputFields] = useState([]); // State to store input fields
     const inputFieldRef = useRef(null); // Ref for input field
-    const [inputField, setInputField] = useState(null); // State to store the position and value of the input field
 
     useEffect(()=> {
         fetchUserDetails(navigate, setUser, setUsersLoading);
     }, []);
 
     useEffect(() => {
-        console.log("Updated position:", inputField);
-    }, [inputField]);
+        console.log("Updated position:", inputFields);
+    }, [inputFields]);
 
     const handleDocumentLoadSuccess = ({ numPages }) => {
         setNumPages(numPages);
@@ -49,130 +49,117 @@ const ViewPdf = () => {
         }
     };
 
-    const addInputField = () => {
-        if (!inputField) {
-            setInputField({ x: 0, y: 0, value: '' });
-        } else {
-            // If an input field already exists, update its position
-            setInputField(prevInputField => ({
-                ...prevInputField,
-                x: 0,
-                y: 0,
-                value: ''
-            }));
-        }
+    const handleAddInputField = () => {
+        setInputFields(prevInputFields => [
+            ...prevInputFields,
+            { x: 0, y: 0, value: '' }
+        ]);
+    };
+
+    const handleDrag = (pageIndex, inputIndex, e, ui) => {
+        setInputFields(prevInputFields => {
+            const updatedInputFields = [...prevInputFields];
+            updatedInputFields[inputIndex] = {
+                ...updatedInputFields[inputIndex],
+                x: ui.x,
+                y: ui.y,
+                pageIndex: pageIndex // Store the page index
+            };
+            return updatedInputFields;
+        });
     };
     
 
-    const handleDrag = (e, ui) => {
-        console.log("this is the hell ui", ui);
-        // if (ui.position) {
-            const x = ui.x;
-            const y = ui.y;
-            setInputField(prevInputField => ({
-                ...prevInputField,
-                x,
-                y
-            }));
-        // }
+    const handleInputChange = (index, value) => {
+        setInputFields(prevInputFields => {
+            const updatedInputFields = [...prevInputFields];
+            updatedInputFields[index] = {
+                ...updatedInputFields[index],
+                value: value
+            };
+            return updatedInputFields;
+        });
     };
-    
 
-    const handleDragStart = () => {
-        // Ensure the input field remains visible during dragging by setting its zIndex higher
-        setInputField(prevInputField => ({
-            ...prevInputField,
-            zIndex: 9999 // Set a high zIndex value
-        }));
-    };
-    
-    const handleDragStop = (e, ui) => {
-        if (ui.position) {
-            const { x, y } = ui.position;
-            console.log("New position (x, y):", x, y); // Log the new position
-            setInputField(prevInputField => ({
-                ...prevInputField,
-                x,
-                y
-            }));
-        }
-    };
-    
-    
-
-    return(
+    return (
         <>
-        {usersLoading ? 
-            (
-                <Spinner 
-                    position= "fixed"
-                    top= "50%"
-                    left= "50%"
-                    size = "xl"
-                    transform= "translate(-50%, -50%)"
-                />
-            ) : (
-                <Sidebar>
-                    <Flex
-                        p="12px"
-                        justify="space-between"
-                        bg="grey"
-                        maxH={"4em"}
-                    >
-                        <Text display={"inline-block"}>{pageNumber} of {numPages}</Text>
-                        <Button
-                            colorScheme='twitter'
-                            rightIcon={<FaDownload />}
-                            onClick={handleDownload}
+            {usersLoading ? 
+                (
+                    <Spinner 
+                        position= "fixed"
+                        top= "50%"
+                        left= "50%"
+                        size = "xl"
+                        transform= "translate(-50%, -50%)"
+                    />
+                ) : (
+                    <Sidebar>
+                        <Flex
+                            p="12px"
+                            justify="space-between"
+                            bg="grey"
+                            maxH={"4em"}
                         >
-                            Download
-                        </Button>
-                        <Button onClick={addInputField}>Add Input</Button> {/* Button to add input field */}
-                    </Flex>
-                    <Flex
-                        bg="#00000099"
-                        display="flex"
-                        justifyContent="center"
-                        userSelect="none"
-                        alignItems="center"
-                    >
-                        <Document
-                            file={`http://localhost:3001/pdf/${user.id}/pdfs/${id}`}
-                            onLoadSuccess={handleDocumentLoadSuccess}
+                            <Text display={"inline-block"}>{pageNumber} of {numPages}</Text>
+                            <Button
+                                colorScheme='twitter'
+                                rightIcon={<FaDownload />}
+                                onClick={handleDownload}
+                            >
+                                Download
+                            </Button>
+                            <Button onClick={handleAddInputField}>Add Input</Button> {/* Button to add input field */}
+                        </Flex>
+                        <Flex
+                            bg="#00000099"
+                            display="flex"
+                            justifyContent="center"
+                            userSelect="none"
+                            alignItems="center"
                         >
-                            {Array.from(new Array(numPages), (el, index) => (
-                                <Box
-                                    my="12px"
-                                    key={`page_${index + 1}`}
-                                >
-                                    <Page  pageNumber={index + 1} className={"page"} />
-                                    {/* Render input field if it exists */}
-                                    {inputField && (
-                                        <Draggable
-                                            // onStart={handleDragStart}
-                                            // onStop={handleDragStop}
-                                            onDrag={handleDrag}
-                                            defaultPosition={{ x: inputField.x, y: inputField.y }}
-                                        >
-                                            <Input 
-                                                ref={inputFieldRef}
-                                                zIndex={2} 
-                                                size="xs"
-                                                w="xs"
-                                                placeholder={`x: ${inputField.x} y: ${inputField.y} at ${index + 1} page`}
-                                                style={{ position: 'absolute', left: inputField.x, top: inputField.y }}
-                                                value={inputField.value}
-                                                onChange={(e) => setInputField(prevInputField => ({ ...prevInputField, value: e.target.value }))}
-                                            />
-                                        </Draggable>
-                                    )}
-                                </Box>
-                            ))}
-                        </Document>
-                    </Flex>
-                </Sidebar>
-            )
-        }
+                            <Document
+                                file={`http://localhost:3001/pdf/${user.id}/pdfs/${id}`}
+                                onLoadSuccess={handleDocumentLoadSuccess}
+                            >
+                                {Array.from(new Array(numPages), (el, index) => (
+                                    <Box
+                                        my="12px"
+                                        key={`page_${index + 1}`}
+                                    >
+                                        <Page  pageNumber={index + 1} className={"page"} />
+                                        {/* Render input field if it exists for the current page */}
+                                        {inputFields && inputFields.map((inputField,inputIndex) => (
+                                            <Draggable
+                                            onDrag={(e, ui) => handleDrag(index, inputIndex, e, ui)}
+                                            defaultPosition={{
+                                                    x: inputField.x,
+                                                    y: inputField.y
+                                                }}
+                                            >
+                                                <Input
+                                                    zIndex={2}
+                                                    size="xs"
+                                                    w="xs"
+                                                    placeholder={`x: ${inputField.x} y: ${inputField.y} at ${index + 1} page`}
+                                                    style={{
+                                                        position: 'absolute',
+                                                        left: inputField.x,
+                                                        top: inputField.y
+                                                    }}
+                                                    value={inputField.value}
+                                                    onChange={e =>
+                                                        handleInputChange(index, e.target.value)
+                                                    }
+                                                />
+                                            </Draggable>
+                                        ))}
+                                    </Box>
+                                ))}
+                            </Document>
+                        </Flex>
+                    </Sidebar>
+                )
+            }
         </>
     );
 };

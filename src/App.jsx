9b39a64a31d1f57212308663b6118c228c527658
Pranav Soni <pranavsoni1101@gmail.com>
2024-02-26 15:08:09 +0530

    Changed Home page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import { Heading, Text } from '@chakra-ui/react';
 import React from 'react';
+import {
+  Flex,
+  Container,
+  Heading,
+  Stack,
+  Text,
+  Button,
+} from '@chakra-ui/react'
 import { pdfjs } from 'react-pdf';
+import Illustration from '../components/Illustration';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
@@ -8,12 +16,48 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 ).toString();
 
 const App = () => {
-    return(
-        <>
-            <Heading>This is Home</Heading>
-            <Text>Hss</Text>
-        </>
-    )
-}
+  return (
+    <Container maxW={'5xl'}>
+      <Stack
+        textAlign={'center'}
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}>
+        <Heading
+          fontWeight={600}
+          textTransform="capitalize"
+          fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+          lineHeight={'110%'}>
+          Document Signing{' '}
+          <Text as={'span'} color={'orange.400'}>
+            made easy
+          </Text>
+        </Heading>
+        <Text color={'gray.500'} maxW={'3xl'}>
+            Never miss a signing session. 
+            Never be late for one either. Keep 
+            track of your signings and receive smart 
+            reminders at the right times.
+        </Text>
+        <Stack spacing={6} direction={'row'}>
+          <Button
+            rounded={'full'}
+            px={6}
+            colorScheme={'orange'}
+            bg={'orange.400'}
+            _hover={{ bg: 'orange.500' }}>
+            Get started
+          </Button>
+          <Button rounded={'full'} px={6}>
+            Learn more
+          </Button>
+        </Stack>
+        <Flex w={'full'}>
+          <Illustration height={{ sm: '24rem', lg: '28rem' }} mt={{ base: 12, sm: 16 }} />
+        </Flex>
+      </Stack>
+    </Container>
+  )
+};
 
 export default App;
